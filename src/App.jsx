4811cf9464b5b9c7496022babeadd92a4dad43d8
zuhebms <83272858+zuhebms@users.noreply.github.com>
2024-01-07 00:09:53 +0530

    tic-tac-toe

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Player from "./components/Player";
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        Game Board
+      </div>
+    </main>
+  );
+}
+
+export default App;
